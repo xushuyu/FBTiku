6bd415c7a235a2eeb04bf3d7d154138cca2c3d8e
garyx <xushuyu1987@gmail.com>
2018-01-09 23:37:16 -0800

    Update Cmake

---
+++ cmake-build-debug/CMakeFiles/FBTiku.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FBTiku.dir/main.cpp.obj"
+  "FBTiku.pdb"
+  "FBTiku.exe"
+  "FBTiku.exe.manifest"
+  "FBTiku.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FBTiku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
